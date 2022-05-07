30e4eda854559106a8371d8157a4912d9b7467f6
mimoislam <benoaie.islam@gmail.com>
2022-05-07 08:33:46 +0100

    added the communication added the form added the creation host file

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app);
-const WebSocket = require('ws');
+
+var http = require('http');
 
 
 var shell=require("./shellFunction")
@@ -10,7 +8,6 @@ const {default: simpleGit, CleanOptions} = require('simple-git');
 simpleGit().clean(CleanOptions.FORCE);
 
 
-console.log(process.cwd());
 
 
 
@@ -26,48 +23,92 @@ console.log(process.cwd());
 
 
 
-const wss = new WebSocket.Server({ server:server });
+// const wss = new WebSocket.Server({ server:server });
 
-let  status=status_TYPE.LIBRE;
-module.exports=status;
+// let  status=status_TYPE.LIBRE;
+// module.exports=status;
 
-wss.on('connection', function connection(ws) {
+// wss.on('connection', function connection(ws) {
 
-  console.log('A new client Connected!');
-  /// send the status of this  to the new connection  established 
-  ws.send(
-    JSON.stringify({
-      'status':status
-    })
+//   console.log('A new client Connected!');
+//   /// send the status of this  to the new connection  established 
+//   ws.send(
+//     JSON.stringify({
+//       'status':status
+//     })
     
-    );
+//     );
   
-  ws.on('close', function() {
+//   ws.on('close', function() {
 
-    console.log('Connection Closed');
+//     console.log('Connection Closed');
   
+//   });
+
+// //// when connection is closed  for this client 
+//     ws.on('close', function() {
+//         console.log('Connection Closed');
+//     });
+//     /// when client sends message 
+//   ws.on('message', function incoming(message) { 
+//     status=status_TYPE.OCCUPIED;
+    
+//     shell.shell('https://github.com/mimoislam/gamma');
+//      wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(JSON.stringify({
+//           'status':status
+//         }));
+//       }
+//     });
+
+//   });
+// });
+var sendInterval = 5000;
+
+function sendServerSendEvent(req, res) {
+  res.writeHead(200, {
+  'Content-Type' : 'text/event-stream',
+  'Cache-Control' : 'no-cache',
+  'Connection' : 'keep-alive',
+  'Access-Control-Allow-Credentials': true
   });
+  
+  
+  setInterval(function() {
+  writeServerSendEvent(res,{status:0});
+  }, sendInterval);
+  
+  writeServerSendEvent(res, {status:0});
+ }
+  
+ function writeServerSendEvent(res, data) {
+  res.write("data:" + JSON.stringify(data) + '\n\n');
+ }
+ let  status=status_TYPE.LIBRE;
+http.createServer(function(req, res) {
 
-//// when connection is closed  for this client 
-    ws.on('close', function() {
-        console.log('Connection Closed');
-    });
-    /// when client sends message 
-  ws.on('message', function incoming(message) { 
+  if (req.url == '/status') {
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+     res.setHeader('Access-Control-Allow-Credentials', true);
+  sendServerSendEvent(req, res);
+  } else{
+    if (req.url == '/start') {
+
+      
     status=status_TYPE.OCCUPIED;
-    
     shell.shell('https://github.com/mimoislam/gamma');
-    /* wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({
-          'status':status
-        }));
-      }
-    });*/
 
-  });
-});
 
-app.get('/', (req, res) => res.send('Hello World!'))
+   }  else {
+    res.writeHead(404);
+    res.end();
+    }
+  }
+ 
+  
+ }).listen(3000);
 
-server.listen(3000, () => console.log(`Lisening on port :3000`))
+
--- shellFunction.js
+++ shellFunction.js
@@ -3,6 +3,9 @@ var status_TYPE=require("./Status");
 var status=require('./app');
 var createRepo = require( '@moyuyc/github-create-repo' );
 const git = require('simple-git')()
+const fs = require('fs');
+
+
 
 
 
@@ -45,12 +48,74 @@ await git.clone(repoURL, localPath, options, handlerFn());
 }
 
 
+function createHostFile(data,groupe_name){
+	var content = "["+groupe_name+"]";
 
+	data.forEach(function (element ){
+		content=content+"\n";
+
+
+		var ipAddr=element .ip;
+		content=content+ipAddr+"  "
+
+		var user=element .user;
+		content=content+"ansible_user="+user+"  "
+
+
+
+		var password=element .password;
+		content=content+"ansible_password="+password+"  "
+
+		var privilege=element.privilege;
+		if(privilege){
+
+			var become_user=element.become_user;
+			content=content+"ansible_become_user="+become_user+"  "
+
+
+			var become_password=element.become_password;
+			content=content+"ansible_become_pass="+become_password+"  "
+
+
+		}
+
+		
+		
+
+	})
+	return content;
+}
 
 
 async function shell(gitUrl){
-    gitClone(gitUrl)
+	groupe_name="linux"
+	data=[{
+		ip:'192.168.1.44',
+		user:'kali',
+		password:'kali',
+		privilege:true,
+		become_user:'root',
+		become_password:'kali',
+	},{
+		ip:'192.168.1.1',
+		user:'kali2',
+		password:'kali2',
+		privilege:true,
+		become_user:'root',
+		become_password:'kali',
+	}]
+    //gitClone(gitUrl)
 
+
+	try {
+	  fs.writeFile('hosts', createHostFile(data,groupe_name),(err) => {
+		if (err)
+			throw err;
+		console.log('File saved!');
+	})	  // file written successfully
+	} catch (err) {
+	  console.error(err);
+	}
 
 
 
